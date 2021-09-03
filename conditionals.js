43aa523ff0f0575016e0f8d7abd0d71244734573
AJ <aj@smartnvm.com>
2021-09-03 14:34:04 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -6,15 +6,36 @@ if (raining) {
   console.log("Don't forget your umbrella!");
 }
 
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
 if (cold) {
-  
-if (temperature < 0) {
+  if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
+  } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
-} else {
+  } else {
   console.log("Short sleeves are fine.");
+  }
 }
-}
+
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}  
+
+
+
+//*******************
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
 
