2dd1da9b18592c810432f1db831a8d72ab5405b9
AJ <aj@smartnvm.com>
2021-09-04 17:02:15 +0000

    sync file

--- hello.js
+++ hello.js
@@ -7,3 +7,15 @@ sayHello();
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('return John');
+
+console.log(greeting);
